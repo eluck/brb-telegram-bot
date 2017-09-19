e6b368503f05e79883e402bebd7494d6095acd6c
eluck <mail@eluck.me>
2017-09-19 11:30:34 +0300

    Add bot restart button for development mode

--- imports/menus/send_menu.js
+++ imports/menus/send_menu.js
@@ -1,13 +1,14 @@
 const { Markup } = require('micro-bot');
+const isDevelopment = require('/imports/lib/is-development');
 
 
 module.exports = (ctx) => {
-  ctx.reply(ctx.i18n.t('common.menuMessage'), Markup
-    .keyboard([
-      [ctx.i18n.t('common.menu.coffee'), ctx.i18n.t('common.menu.altcoffee')],
-      [ctx.i18n.t('common.menu.tea'), ctx.i18n.t('common.menu.coctails')],
-      [ctx.i18n.t('common.menu.breakfast'), ctx.i18n.t('common.menu.decerts')],
-      [ctx.i18n.t('common.menu.order')]])
-    .resize()
-    .extra());
+  const menu = [
+    [ctx.i18n.t('common.menu.coffee'), ctx.i18n.t('common.menu.altcoffee')],
+    [ctx.i18n.t('common.menu.tea'), ctx.i18n.t('common.menu.coctails')],
+    [ctx.i18n.t('common.menu.breakfast'), ctx.i18n.t('common.menu.decerts')],
+    [ctx.i18n.t('common.menu.order')]
+  ];
+  if (isDevelopment) menu.unshift(['/start']);
+  ctx.reply(ctx.i18n.t('common.menuMessage'), Markup.keyboard(menu).resize().extra());
 };
